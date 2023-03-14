2a1d6c787b3d8ef8fd8786a60fb57dea7c3fdf52
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-14 18:41:31 -0300

    UDATED: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,26 @@ import styles from './styles.module.css'
 export default function Footer() {
     return (
         <div
-            className={styles.div}
+            className={styles.footer}
         >
-            Footer
+            <div
+                className={styles.div}
+            >
+                <p className={styles.p}>Precisa de ajuda, tem dúvidas ou sugestões? Escreve pra <a href='mailto:devall@itexto.com.br' className={styles.a}>gente!</a></p>
+            </div>
+            <div
+                className={styles.div}
+            >
+                <i className='bx bxl-linkedin-square' ></i>
+                <i className='bx bxl-twitter' ></i>
+                <i className='bx bxl-facebook-square' ></i>
+                <i className='bx bxl-github' ></i>
+            </div>
+            <div
+                className={styles.div}
+            >
+                <p className={styles.p}>Desenvolvido por <a href='https://itexto.com.br/' className={styles.iTexto}><span className={styles.red}>i</span>texto</a></p>
+            </div>
         </div>
     )
 }
