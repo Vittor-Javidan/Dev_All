f4bdd993bb6bc427308576e451c09750d1a18fca
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-15 07:42:01 -0300

    FIX: Navbar now have fixed position.
    	- Was necessary to remove the padding of the main body element,
    	  and add fake backgrounds on both footer and navbar, to
    	  simulate a padding.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,26 +2,28 @@ import styles from './styles.module.css'
 
 export default function Footer() {
     return (
-        <div
-            className={styles.footer}
-        >
+        <div className={styles.fakeBackground}>
             <div
-                className={styles.div}
+                className={styles.footer}
             >
-                <p className={styles.p}>Precisa de ajuda, tem dúvidas ou sugestões? Escreve pra <a href='mailto:devall@itexto.com.br' className={styles.a}>gente!</a></p>
-            </div>
-            <div
-                className={styles.div}
-            >
-                <i className='bx bxl-linkedin-square' ></i>
-                <i className='bx bxl-twitter' ></i>
-                <i className='bx bxl-facebook-square' ></i>
-                <i className='bx bxl-github' ></i>
-            </div>
-            <div
-                className={styles.div}
-            >
-                <p className={styles.p}>Desenvolvido por <a href='https://itexto.com.br/' className={styles.iTexto}><span className={styles.red}>i</span>texto</a></p>
+                <div
+                    className={styles.div}
+                >
+                    <p className={styles.p}>Precisa de ajuda, tem dúvidas ou sugestões? Escreve pra <a href='mailto:devall@itexto.com.br' className={styles.a}>gente!</a></p>
+                </div>
+                <div
+                    className={styles.div}
+                >
+                    <i className='bx bxl-linkedin-square' ></i>
+                    <i className='bx bxl-twitter' ></i>
+                    <i className='bx bxl-facebook-square' ></i>
+                    <i className='bx bxl-github' ></i>
+                </div>
+                <div
+                    className={styles.div}
+                >
+                    <p className={styles.p}>Desenvolvido por <a href='https://itexto.com.br/' className={styles.iTexto}><span className={styles.red}>i</span>texto</a></p>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,86 +6,88 @@ export default function Navbar() {
     const [toggle, setToggle] = useState(false)
 
     return (
-        <div className={styles.div}>
-            <h1 className={styles.h1}>/dev/All</h1>
-            <label 
-                className={styles.label}
-                onClick={() => setToggle(prev => !prev)}
-            >
-                <i 
-                    className={`
-                        bx bx-menu
-                        ${styles.i}
-                    `}
-                ></i>
-            </label>
-            <ul className={`
-                ${styles.ul}
-                ${!toggle && styles.ulDisable}
-            `}>
-                <li 
-                    className={styles.li}
+        <div className={styles.fakeBackground}>
+            <div className={styles.div}>
+                <h1 className={styles.h1}>/dev/All</h1>
+                <label 
+                    className={styles.label}
+                    onClick={() => setToggle(prev => !prev)}
                 >
-                    <a 
+                    <i 
                         className={`
-                            ${styles.a}
-                            ${styles.discord}
-                        `} 
-                        href="https://discord.com/invite/JyXzTcP"
-                        target={"_blank"}
+                            bx bx-menu
+                            ${styles.i}
+                        `}
+                    ></i>
+                </label>
+                <ul className={`
+                    ${styles.ul}
+                    ${!toggle && styles.ulDisable}
+                `}>
+                    <li 
+                        className={styles.li}
                     >
-                        DISCORD
-                    </a>
-                </li>
-                <li 
-                    className={styles.li}
-                >
-                    <a 
-                        className={`
-                            ${styles.a}
-                            ${styles.taverna}
-                        `} 
-                        href=""
+                        <a 
+                            className={`
+                                ${styles.a}
+                                ${styles.discord}
+                            `} 
+                            href="https://discord.com/invite/JyXzTcP"
+                            target={"_blank"}
+                        >
+                            DISCORD
+                        </a>
+                    </li>
+                    <li 
+                        className={styles.li}
                     >
-                        TAVERNA
-                    </a>
-                </li>
-                <li className={styles.li}>
-                    <a 
-                        className={styles.a}
-                    >
-                        REVISTA
-                    </a>
-                </li>
-                <li className={styles.li}>
-                    <a 
-                        className={styles.a}
-                    >
-                        BLOGS
-                    </a>
-                </li>
-                <li className={styles.li}>
-                    <a 
-                        className={styles.a}
-                    >
-                        SOBRE
-                    </a>
-                </li>
-                <li className={styles.li}>
-                    <a 
-                        className={styles.a}
-                    >
-                        CADASTRE-SE!
-                    </a>
-                </li>
-                <li className={styles.li}>
-                    <a 
-                        className={styles.a}
-                    >
-                        ENTRAR
-                    </a>
-                </li>
-            </ul>
+                        <a 
+                            className={`
+                                ${styles.a}
+                                ${styles.taverna}
+                            `} 
+                            href=""
+                        >
+                            TAVERNA
+                        </a>
+                    </li>
+                    <li className={styles.li}>
+                        <a 
+                            className={styles.a}
+                        >
+                            REVISTA
+                        </a>
+                    </li>
+                    <li className={styles.li}>
+                        <a 
+                            className={styles.a}
+                        >
+                            BLOGS
+                        </a>
+                    </li>
+                    <li className={styles.li}>
+                        <a 
+                            className={styles.a}
+                        >
+                            SOBRE
+                        </a>
+                    </li>
+                    <li className={styles.li}>
+                        <a 
+                            className={styles.a}
+                        >
+                            CADASTRE-SE!
+                        </a>
+                    </li>
+                    <li className={styles.li}>
+                        <a 
+                            className={styles.a}
+                        >
+                            ENTRAR
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
